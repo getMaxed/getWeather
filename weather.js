77bb07c00275950d212bc0c1420683268d29aaea
getmaxed <maxfaden07@gmail.com>
2018-02-08 12:43:33 -0800

    Weather class created, fetching the API data into the console

---
+++ weather.js
@@ -0,0 +1,21 @@
+class Weather {
+    constructor(city, state) {
+        this.apiKey = 'f4aa497e310296c8';
+        this.city = city;
+        this.state = state;
+    }
+
+    //Fetching weather from API
+    async getWeather() {
+        const response = await fetch(`http://api.wunderground.com/api/${this.apiKey}/conditions/q/${this.state}/${this.city}.json`);
+
+        const responseData = await response.json();
+        return responseData.current_observation;
+    }
+
+    //Change weather location
+    changeLocation(city, state) {
+        this.city = city;
+        this.state = state;
+    }
+}
