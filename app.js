fa4e45e2815d99c405ecafc99d3daa525c1a9593
getmaxed <maxfaden07@gmail.com>
2018-02-09 13:04:45 -0800

    UI class created, displaying the data

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 const weather = new Weather('Oakland', 'CA');
-
+const ui = new UI();
 document.addEventListener('DOMContentLoaded', getWeather);
+
+document.getElementById('w-change-btn').addEventListener('click', (e) => {
+    const city = document.getElementById('city').value;
+    const state = document.getElementById('state').value;
+
+    weather.changeLocation('Miami', 'FL');
+
+    getWeather();
+
+    $('#locModal').modal('hide');
+});
 
 function getWeather() {
     weather.getWeather()
         .then(results => {
-            console.log(results);
+            ui.paint(results);
         })
         .catch(err => console.log(err));
 }
