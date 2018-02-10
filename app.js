9f0673168aa02b3ed9b662c80e4ad4d96f2e8253
getmaxed <maxfaden07@gmail.com>
2018-02-10 11:14:23 -0800

    Saving location into the Local Storage

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const weather = new Weather('Oakland', 'CA');
 const ui = new UI();
+const storage = new Storage();
+const weatherLocation = storage.getLocationData();
+const weather = new Weather(weatherLocation.city, weatherLocation.state);
+
+
 document.addEventListener('DOMContentLoaded', getWeather);
 
 document.getElementById('w-change-btn').addEventListener('click', (e) => {
     const city = document.getElementById('city').value;
     const state = document.getElementById('state').value;
 
-    weather.changeLocation('Miami', 'FL');
+    // Change Location in Local Storage
+    weather.changeLocation(city, state);
+
+    // Change Location in Local Storage
+    storage.setLocationData(city, state);
 
     getWeather();
 
